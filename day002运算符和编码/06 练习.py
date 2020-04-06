6986aab0b5098e5d2a46b4708a1981fd10ed7535
jawy <honourjawy@163.com>
2020-04-06 21:42:16 +0800

    Increase the analysis of finished product analysis.

---
+++ day002运算符和编码/06 练习.py
@@ -0,0 +1,56 @@
+# while True:
+#     print('狼的诱惑')
+
+
+# '''
+# flag = True
+# while flag:
+#     print('呵呵哒')
+# '''
+
+
+
+# count = 1
+# flag = True
+# while flag:
+#     print(count)
+#     count = count + 1
+#     if count == 101:
+#         flag = False
+
+
+
+# count = 1
+# while count < 101:
+#     print(count)
+#     count = count + 1
+
+
+# s = 0
+# count = 1
+# while count < 101:
+#     s = s + count
+#     count = count + 1
+# print(s)
+
+
+
+# while True:
+#     print('狼的诱惑')
+#     print('我们不一样')
+#     print('月亮之上')
+#     break
+#     print('庐州月')
+#     print('人间')
+# print(111)
+
+
+
+#1^100所有的偶数:%
+print( 102 % 2 )
+
+
+
+
+
+
