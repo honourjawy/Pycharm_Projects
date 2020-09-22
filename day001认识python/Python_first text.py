1e8cc78638a214e7a61def3f2e4327c92a910e9d
jawy <honourjawy@163.com>
2020-09-22 13:35:11 +0800

    Increase the analysis of finished product analysis.

--- day001认识python/Python_first text.py
+++ day001认识python/Python_first text.py
@@ -20,13 +20,51 @@
 #     print('你的姓名有误!')
 
 
-code = 'abcd'
-username = input('请输入姓名:')
-your_code = input('请输入密码:')
-if username == 'jawy':
-    if your_code == code:
-        print('登陆成功')
-    else:
-        print('密码错误')
-else:
-    print('你的姓名有误!')
+# code = 'abcd'
+# username = input('请输入姓名:')
+# your_code = input('请输入密码:')
+# if username == 'jawy':
+#     if your_code == code:
+#         print('登陆成功')
+#     else:
+#         print('密码错误')
+# else:
+#     print('你的姓名有误!')
+
+# s = 0
+# count = 0
+# while s < 100:
+#     s+=1
+#     count = count + s
+# print(count)
+
+
+
+# # msg = int(input('请输入年龄:'))
+# # name = input('请输入姓名:')
+# # print('我叫%s,我今年%d岁了') % (name,msg)
+# # # 错在哪里了
+#
+# msg = int(input('请输入年龄:'))
+# name = input('请输入姓名:')
+# qingkuang='我叫%s,我今年%d岁了.' % (name,msg)
+# print(qingkuang)
+
+
+
+# yourname = 'jawy'
+# yourpassword = '123'
+# count = 1
+# while count <= 3:
+#     name = input('请输入姓名:')
+#     password = input('请输入密码:')
+#     if name == yourname:
+#         if password == yourpassword:
+#             print('成功登陆')
+#             break
+#         else:
+#             print('密码错误')
+#     else:
+#         print('姓名输入错误')
+#     count+=1
+
