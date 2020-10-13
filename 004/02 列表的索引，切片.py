fd6cfd34c483a2bec0cf4a2c1ffe0f7a74671796
jawy <honourjawy@163.com>
2020-10-13 14:03:52 +0800

    Increase the analysis of finished product analysis.

--- 004/02 列表的索引，切片.py
+++ 004/02 列表的索引，切片.py
@@ -1,15 +1,15 @@
 li = [100, '太白', True, [1, 2, 3]]
 # 索引
-# print(li[0], type(li[0]))
-# print(li[1],type(li[1]))
-# print(li[-1])
+print(li[0], type(li[0]))      # 100 <class 'int'>
+print(li[1],type(li[1]))       # 太白 <class 'str'>
+print(li[-1],type(li[-1]))     # [1, 2, 3] <class 'list'>
 
 # 切片 （顾头不顾腚）
-# print(li[:2])
+print(li[:2])                  # [100, '太白']
 
 
 # 练习题
-li = [1, 3, 2, "a", 4, "b", 5, "c"]*
+# li = [1, 3, 2, "a", 4, "b", 5, "c"]*
 # 通过对li列表的切片形成新的列表l1,l1 = [1,3,2]
 # 通过对li列表的切片形成新的列表l2,l2 = ["a",4,"b"]
 # print(li[3:6])
--- 成品_分析与学习/正态分布.py
+++ 成品_分析与学习/正态分布.py
@@ -1,41 +1,53 @@
-# #
-# # 本文以某一批产品的长度为数据集
-# # 在此数据集的基础上绘制直方图和正态分布曲线
-# #
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+def normfun(x, pingjun, biaozhuncha):
+    fx = 1 / (biaozhuncha * np.sqrt(2 * np.pi))*(np.exp(-((x - pingjun) ** 2) / (2 * (biaozhuncha ** 2))))
+    return fx
+if __name__ == '__main__':
+    data = pd.read_csv('jawy.csv')
+    length = data['length']
+    mean = length.mean()
+    std = length.std()
+    x = np.arange(3622, 3656, 0.1)
+    y = normfun(x, mean, std)
+    plt.plot(x, y)
+    plt.hist(length, bins=16, rwidth=0.75,facecolor='blue', density=True)
+    plt.title('Normal distribution')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
+else:
+    print('运行错误')
+
+
+
 #
-# import pandas as pd  # pandas是一个强大的分析结构化数据的工具集
-# import numpy as np  # numpy是Python中科学计算的核心库
-# import matplotlib.pyplot as plt  # matplotlib数据可视化神器
-#
-# # 正态分布的概率密度函数
-# #   x      数据集中的某一具体测量值
-# #   mu     数据集的平均值，反映测量值分布的集中趋势
-# #   sigma  数据集的标准差，反映测量值分布的分散程度
-# def normfun(x, mu, sigma):
-#     pdf = np.exp(-((x - mu) ** 2) / (2 * sigma ** 2)) / (sigma * np.sqrt(2 * np.pi))
-#     return pdf
-#
-# if __name__ == '__main__':
-#
-#     data = pd.read_csv('./le.csv') # 载入数据文件
-#     print(data)
-#     length = data['length'] # 获得长度数据集
-#     mean = length.mean() # 获得数据集的平均值
-#     std = length.std()   # 获得数据集的标准差
-#
-#     # 设定X轴：前两个数字是X轴的起止范围，第三个数字表示步长
-#     # 步长设定得越小，画出来的正态分布曲线越平滑
-#     x = np.arange(3613, 2553, 0.1)
-#     # 设定Y轴，载入刚才定义的正态分布函数
-#     y = normfun(x, mean, std)
-#     # 绘制数据集的正态分布曲线
-#     plt.plot(x, y)
-#
-#     # 绘制数据集的直方图
-#     plt.hist(length, bins=12, rwidth=0.9, density=True)
-#     plt.title('Length distribution')
-#     plt.xlabel('Length')
-#     plt.ylabel('Probability')
-#
-#     # 输出正态分布曲线和直方图
-#     plt.show()
+# import math
+# import numpy as np
+# import matplotlib.pyplot as plt
+# b = 5                 # 求取均值μ
+# a = math.sqrt(0.6)    # 标准差δ
+# x_zhou = np.linspace(b - 3*a, b + 3*a, 50)   # 在指定的间隔内返回均匀间隔的数字。
+# y_zhou = np.exp(-(x_zhou - b) ** 2 /(2* a **2))/(math.sqrt(2*math.pi)*a)   #正态密度
+# plt.plot(x_zhou, y_zhou, "r-", linewidth=4)   #图形设置
+# plt.grid(True)
+# plt.show()    # 作图形
+
+
+
+
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import math
+# u = 4
+# sig = math.sqrt(0.6)
+# x = np.linspace(u - 3 * sig, u + 3 * sig, 1000)
+# y_sig = np.exp(-(x - u) ** 2 / (2 * sig ** 2)) / (math.sqrt(2 * math.pi) * sig)
+# plt.plot(x, y_sig, "r-", linewidth=2)
+# plt.grid(True)
+# plt.title('Normal distribution')
+# plt.show()
+
+
+
